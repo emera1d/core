657feb5410b3fcfcb40828d25acf7ff4629c787e
Ivan Nazarov <i.nazarov@corp.badoo.com>
2014-09-25 14:01:28 +0400

    new meta

--- qaapi/js/qaapi_meta.js
+++ qaapi/js/qaapi_meta.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var _U_ID_ = {
+	cookie: 'aid'
+};
+
 var qa_meta = {
 	accessLinkGet: {
 		url: 'accessLinkGet'
 		, params: {
-			user_id: {
-				cookie: 'aid'
-			}
+			user_id: _U_ID_
 			, context: {}
 			, partner_id: {}
 		}
@@ -15,9 +17,7 @@ var qa_meta = {
 	, userVerifyPhone: {
 		url: 'userVerifyPhone'
 		, params: {
-			user_id: {
-				cookie: 'aid'
-			}
+			user_id: _U_ID_
 			, phone: {
 				initial: '1234567890'
 			}
@@ -27,29 +27,132 @@ var qa_meta = {
 	, userCleanTrustedNetwork: {
 		url: 'userCleanTrustedNetwork'
 		, params: {
-			user_id: {
-				cookie: 'aid'
-			}
+			user_id: _U_ID_
+		}
+	}
+
+	, chatSendMessage: {
+		url: 'chatSendMessage'
+		, params: {
+			user_id: _U_ID_
+			, contact_user_id: {}
+			, message: { initial: 'hello' }
+		}
+	}
+
+	, userAddCredits: {
+		url: 'userAddCredits'
+		, params: {
+			user_id: _U_ID_
+			, amount: {}
+		}
+	}
+
+	, userRemoveCredits: {
+		url: 'userRemoveCredits'
+		, params: {
+			user_id: _U_ID_
+			, amount: {}
 		}
 	}
 
 	, userDetachFacebook: {
 		url: 'userDetachFacebook'
 		, params: {
-			user_id: {
-				cookie: 'aid'
-			}
+			user_id: _U_ID_
 		}
 	}
 
 	, userAddSppSubscription: {
 		url: 'userAddSppSubscription'
 		, params: {
-			user_id: {
-				cookie: 'aid'
-			}
+			user_id: _U_ID_
 			, aggregator_id: {}
 		}
 	}
-
 };
+
+/*
+
+userGet
+	Retrieve users' data
+	Return array 
+params:
+	search (string)  – Can be id, email or phone number
+
+
+hitAdd
+	Add hit from one user to another
+	Return bool 
+params:
+	user_id (int)
+	user_id_from (int)
+	is_visible (bool)  – optional, default = false
+	from (string)  – optional, default = ''
+
+
+hitDelete
+	Delete hit
+	Return bool 
+params:
+	user_id (int)
+	user_id_from (int)
+
+
+
+laccessOffline
+	Make user offline
+	Return array|mixed
+params:
+	user_id (int)
+ 
+
+laccessOnline
+	Make user online
+	Return array|mixed 
+params:
+	user_id (int)
+
+
+photoModerate
+	Moderate photo for user
+	Return array 
+params:
+	user_id (int)
+	photo_id (int)
+	moderation_status (string)  – optional set of Approved, ApprovedBad, Trash, Erotic, Other, Friends, Children. Default value is "Approved".
+	forced (bool)  – optional, default = false
+
+
+photoUpload
+	Uploads photo for user
+	Return array 
+params:
+	user_id (int)
+	photo (string)  – url or base64 encoded image data
+	entry_id (string)  – can be "me", "other", "private" (AlbumTriad::getClasses()) or exactly id of album. If absent, photos uploaded to "photos of me".
+
+
+userAttachPhone
+	Attach phone for user
+	Return array 
+params:
+	user_id (int)
+	phoneNumber (int)  – should be passed with country code
+	is_confirmed (bool)  – optional default true
+
+userDetachPhone
+	Detach phones from user
+	Return array
+params:
+	user_id
+
+userGetPhone
+	Retrieve user's phone
+	Return array|false|null 
+params:
+	user_id
+
+
+*/
+
